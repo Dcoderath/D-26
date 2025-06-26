96adcd7f71bece5a6ba4b853bad33ed71863a119
dcoderath <trivedi2693@gmail.com>
2025-06-26 13:49:35 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Dcoderath-26/',
+  base: '/D-26/', // <== THIS is critical
   plugins: [react()],
-  server: {
-    host: true,  // Listen on all network interfaces, not just localhost
-    port: 3000,  // You can set any port you want
-  },
-});
+})

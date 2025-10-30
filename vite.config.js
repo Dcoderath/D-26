2eff1aafcb525e89342a9ed5064519153077495c
Dcoderath <trivedi2693@example.com>
2025-10-30 21:41:18 +0530

    Updated project files

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // ✅ Use this for GitHub Pages or production if needed
   base: '/D-26/',
-
   plugins: [react()],
-
-  // ✅ Enable local network access
-  server: {
-    host: true,          // allows access from other devices
-    port: 5173,          // you can change this if you want
-  },
 })

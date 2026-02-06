eba0dc1009617f5c0c625d1bea673400028b321a
Dcoderath <trivedi2693@example.com>
2026-02-06 21:49:43 +0530

    Update site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/D-26/',
-  plugins: [react()],
-})
+export default {
+  build: {
+    outDir: 'dist'
+  }
+};
